03968779ebe27031b8b9c7bd589e50a389dcc519
Ahmed <ahmeddieba@gmail.com>
2022-04-23 01:46:53 +0200

    Creating the main functions

---
+++ main.py
@@ -0,0 +1,37 @@
+import requests
+from bs4 import BeautifulSoup
+
+def wuzzuf():
+    pass
+def forasna():
+    pass
+def indeed():
+    pass
+def tanqeeb():
+    pass
+def bayt():
+    pass
+
+# def main():
+#     if wuzzuf_check_box == "yes":
+#         wuzzuf()
+#     else:
+#         pass
+#     if forasna_check_box == "yes":
+#         forasna()
+#     else:
+#         pass
+#     if indeed_check_box == "yes":
+#         indeed()
+#     else:
+#         pass
+#     if tanqeeb_check_box == "yes":
+#         tanqeeb()
+#     else:
+#         pass
+#     if bayt_check_box == "yes":
+#         bayt()
+#     else:
+#         pass
+
+
